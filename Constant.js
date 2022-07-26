f7745345e33a0d8d023966475552283ee1d87418
Ashish Kumar Burnwal <ashish293.dev@gmail.com>
2022-07-26 11:25:47 +0530

    first commit

---
+++ Constant.js
@@ -0,0 +1,4 @@
+export const MID = "LhyZqq80688087373959";
+export const API_URL = "http://192.168.43.226:8080/token.php";
+export const CALLBACK_URL = "https://securegw-stage.paytm.in/theia/paytmCallback?ORDER_ID=";
+export const URL_SCHEME = "paytmMIDLhyZqq80688087373959";
